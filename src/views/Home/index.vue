231d1e9cea0a6ac307103fa86c3d156fa273e121
海洋 丁 <3023265978@qq.com>
2024-11-03 19:16:55 +0800

    基本布局划分

---
+++ src/views/Home/index.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Content from '../Content/index.vue'
+import Title from '../Title/index.vue'
+</script>
+
+<template>
+    <div class="box">
+        <div class="test-title"> 
+            <Title></Title>
+        </div>
+        <div class="test-content">
+            <Content></Content>
+        </div>
+    </div>
+
+</template>
+
+<style>
+.box {
+    padding: 10px;            
+    background-color: #ADD8E6; 
+    width: 100vw;              
+    height: 100vh;            
+    box-sizing: border-box;    
+}
+
+.test-title {
+    width: 100%;          
+    height: 12%;         
+    background-color: black; 
+}
+.test-content {
+    width: 100%;          
+    height: 88%;         
+    background-color: red; 
+}
+
+</style>
---
+++ src/views/Title/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        我是标题（校徽，题目，时间）
+    </div>
+</template>
