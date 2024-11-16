d9701b80186c9e1b0ee2eba669d07809f241ade9
海洋 丁 <3023265978@qq.com>
2024-11-16 18:33:55 +0800

    head区域重构完成

--- src/views/Title/index.vue
+++ src/views/Title/index.vue
@@ -1,20 +1,20 @@
 <template>
     <el-row class="head">
         <el-col :span="8">
-            <el-row justify="center" align="bottom">
+            <el-row justify="center" >
                 <el-col :span="3" :offset="3" class="image-col">
                     <img src="/src/assets/images/xiaohui.png" alt="校徽" class="badge-img">
                 </el-col>
-                <el-col :span="3" class="badge-col">
+                <el-col :span="3" class="image-col">
                     <img src="/src/assets/images/yuanhui.png" alt="院徽" class="badge-img">
                 </el-col>
             </el-row>
         </el-col>
-        <el-col :span="8">
-            <span class="title">“千名学子百企行”就业实践活动</span>
+        <el-col :span="8" class="title">
+            <span class="title-text">“千名学子百企行”就业实践活动</span>
         </el-col>
         <el-col :span="8">
-            <Time></Time>
+            <Time class="time"></Time>
         </el-col>
     </el-row>
 </template>
@@ -36,6 +36,23 @@ import Time from './components/Time.vue'
   max-width: 57px; 
   max-height: 57px; 
   object-fit: contain; 
+}
+.title {
+    color: white;
+    font-size: 40px;
+    font-weight: bold;
+
+    margin-top:-9px ;
+}
+.image-col{
+    margin-top:9px ;
+}
+.title-text{
+    margin-left: 80px;
+}
+.time{
+    margin-left: 85px;
+    margin-top:28px ;
 }
 
 </style>
